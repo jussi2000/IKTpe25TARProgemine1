d876b0638eae2fd6e41000c4d42bc698db666137
kerly1000 <johannat2009@gmail.com>
2026-02-03 15:13:17 +0200

    Dictionary

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dictionary ja foreach");
+
+            //var on muutuja 
+            var domains = new Dictionary<string, string>()
+            {
+                {"fi", "Finland"},
+                {"se", "Sweden"},
+                {"de", "Germany"},
+                {"fr", "France"},
+                {"es", "Spain"}
+            };
+            //kasutate foreachi ja juurde tuleb lisada kolmaa rida
+            int i = 1;
+            foreach (var domain in domains)
+            {
+                Console.WriteLine($"{domain.Key} - {domain.Value} - {i}");
+                i++;
+            }
+        }
+    }
+}
