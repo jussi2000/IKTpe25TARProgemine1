67aea72bb2c1cb9b2166ad2660c06fb768e4ba88
Opilane TTHK <johannat2009@gmail.com>
2026-02-06 09:58:48 +0200

    RegEx

---
+++ RegEx/RegEx/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teeme Regular, Expression harjutuse");
+            Console.WriteLine("-----------------------------------");
+
+            string word = "#CD55C";
+            Console.WriteLine("Hex code: " + word);
+            Console.WriteLine("Kas on regex: " + RegExTest(word));
+
+            //Tee regex, mis on false tulemusega
+            //põhjenda ära, et miks on false
+        }
+
+        public static bool RegExTest(string word)
+        {
+            //RegularExpression kontrollib, kas sisestatav string
+            //vastab nõuetele
+            return Regex.IsMatch(word, @"[#][0-9A-Fa-f]{6}\b");
+        }
+    }
+}
