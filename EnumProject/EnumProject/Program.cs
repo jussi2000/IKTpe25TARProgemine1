a7e5b6819f8b1fc66440fbc9966e52029a2bba65
Opilane TTHK <johannat2009@gmail.com>
2026-02-10 15:13:07 +0200

    EnumProject

---
+++ EnumProject/EnumProject/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Reflection.Emit;
+
+namespace EnumProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enum");
+            //Kasutame enum'it Weekdays ja et
+            //näha oleks Friday'd(Reede peab olema tuvastatud)
+            Console.WriteLine(Weekdays.Friday);
+            //tahame näha friday numbrilist väärtust
+            Console.WriteLine((int)Weekdays.Friday);
+
+            //(int)- castimine e. teisendab andmetüübiks
+            //juhul kui info võib kaduma minna ja ei näita soovitud tulemust
+
+            Console.WriteLine("----------");
+            Console.WriteLine(Colors.Green);
+            Console.WriteLine((int)Colors.Green);
+
+            //tehke uus enum Colors
+            //Väärtused on
+            //Red = 3,
+            //Green = 10,
+            //Blue = 13,
+            //Yellow = 5,
+            //Black = 1,
+            //White = 8
+            //ühe värvi peab konsoolis ära näitma        
+        }
+        enum Weekdays
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        enum Colors
+        {
+            Red = 3,
+            Green = 10,
+            Blue = 13,
+            Yellow = 5,
+            Black = 1,
+            White = 8
+        }
+    }
+}
