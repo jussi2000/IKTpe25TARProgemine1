7967434e094e5b2357df7e3e1dbde0ace6fa4768
Opilane TTHK <johannat2009@gmail.com>
2026-03-12 11:14:15 +0200

    InheritanceVINcode

---
+++ InheritanceVINcode/InheritanceVINcode/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace InheritanceVINcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Teha pärislus
+            //On olemas class nimega Machine
+            //See pärib Cars classi
+            //Saab sisestada masina numbri
+            //konsool annab vastuse: Edukalt sisestatud
+            //VIN kood: VIN koodi nr
+        }
+    }
+}
