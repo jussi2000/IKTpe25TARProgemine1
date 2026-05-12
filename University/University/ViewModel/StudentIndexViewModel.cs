1e76519abe8cf473a6ed092c4db7beeb1193d68d
Opilane TTHK <johannat2009@gmail.com>
2026-05-12 14:05:32 +0300

    StudentIndexViewModel

--- University/University/ViewModel/StudentIndexViewModel.cs
+++ University/University/ViewModel/StudentIndexViewModel.cs
@@ -1,10 +1,20 @@
-﻿namespace University.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace University.ViewModel
 {
     public class StudentIndexViewModel
     {
         public int Id { get; set; }
+        [StringLength(50, MinimumLength = 1)]
         public string LastName { get; set; }
+
+        [StringLength(50, MinimumLength = 1)]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
     }
 }
