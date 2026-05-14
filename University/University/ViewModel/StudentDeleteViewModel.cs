5f2b7facbd250e06466f89fde30ba39053662e65
Opilane TTHK <johannat2009@gmail.com>
2026-05-14 14:21:39 +0300

    ViewModel

--- University/University/ViewModel/StudentDeleteViewModel.cs
+++ University/University/ViewModel/StudentDeleteViewModel.cs
@@ -1,12 +1,23 @@
-﻿namespace University.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace University.ViewModel
 {
     public class StudentDeleteViewModel
     {
         public int Id { get; set; }
-        public string LastName { get; set; }
-        public string FirstMidName { get; set; }
-        public DateTime EnrollmentDate { get; set; }
 
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
         public ICollection<EnrollmentViewModel> EnrollmentsVm { get; set; }
     }
 }
--- University/University/ViewModel/StudentDetailsViewModel.cs
+++ University/University/ViewModel/StudentDetailsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using University.Models;
 
 namespace University.ViewModel
@@ -6,8 +7,17 @@ namespace University.ViewModel
     public class StudentDetailsViewModel
     {
         public int Id { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<EnrollmentViewModel> EnrollmentsVm { get; set; }
