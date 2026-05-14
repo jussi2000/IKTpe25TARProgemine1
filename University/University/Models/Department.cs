5eda0e3090e45ca031429dd8cd83213aa4f87dc6
Opilane TTHK <johannat2009@gmail.com>
2026-05-14 14:55:55 +0300

    Models

---
+++ University/University/Models/Department.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace University.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public int Name { get; set; }
+
+        [Column(TypeName = "Money")]
+        public int Budget { get; set; }
+        public DateTime StartDate { get; set; }
+
+        //? tähendab, et see väli võib olla null, st see eiole kohustuslik
+
+        public int? InstructorId { get; set; }
+
+        public Instructor Administrator { get; set; }
+
+        public ICollection<Course> Courses { get; set; }
+    }
+}
---
+++ University/University/Models/Instructor.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace University.Models
+{
+    public class Instructor
+    {
+        public int Id { get; set; }
+
+        public string LastName { get; set; }
+
+        [Column("FirstName")]
+        public string FirstMidName { get; set; }
+
+        public DateTime EnrollmentDate { get; set; }
+
+        //Mis on ICollection?
+        //ICollection on nimekiri, mis võimaldab hoida mitit objecti.
+
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
+
+        //Miks siin ei kasutata ICollection, Vaid lihtsaltOfficeAssignment?
+
+        //Sest OfficeAssignment on üks-ühele seos Instructoriga, st iga õpetaja
+        //võib omada ainult ühte kontoripinda. Seega ei ole vaja kasutada
+        //ICollectioni, kuna ei ole vaja hoida mitut OfficeAssignment objekti.
+        //Kui kasutaksime ICollectioni, siis see tähendaks, et õpetaja
+        //võiks omada mitut kontoripinda, mis ei ole meie mudelis korrektne.
+
+        public OfficeAssignment OfficeAssignments { get; set; }
+    }
+}
