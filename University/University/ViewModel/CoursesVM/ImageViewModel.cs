a40ccfc56ee4051fe615ca2bcda5c521ee7e8036
Opilane Techno+TLN <johannat2009@gmail.com>
2026-09-01 12:59:31 +0300

    uus university

--- University/University/ViewModel/CoursesVM/ImageViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace University.ViewModel.CoursesVM
-{
-    public class ImageViewModel
-    {
-        public Guid ImageId { get; set; }
-        public string? FilePath { get; set; }
-        public Guid? CourseId { get; set; }
-    }
-}
